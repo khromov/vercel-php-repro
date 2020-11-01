9b07a9eab1137cab7f57ea177546789f203ca8f2
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2020-11-01 04:43:04 +0100

    Test with Slim 4

--- src/index.php
+++ src/index.php
@@ -1,26 +1,46 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-use \Slim\Views\PhpRenderer as PhpRenderer;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App;
-$container = $app->getContainer();
-$container['renderer'] = new PhpRenderer(__DIR__ . "/templates");
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+
 
 /**
- * Main page
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+ 
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
  */
-$app->get('/', function (Request $request, Response $response) {
-    return $this->renderer->render($response, "/pages/test.php", []);
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("/ root route");
+    return $response;
+});
+
+// Define app routes
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
 });
 
 /**
  * Form
  */
-$app->post('/submit', function (Request $request, Response $response) {
-    var_dump($_SERVER['REQUEST_METHOD']);
+$app->post('/submit', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("/submit: " . $_SERVER['REQUEST_METHOD']);
+    return $response;
 });
 
 $app->run();
